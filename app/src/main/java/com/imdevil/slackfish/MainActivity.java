323ae8dd46e2bda5b150474dc25632d39f2e293d
imdevil <516959708@qq.com>
2020-09-20 22:11:51 +0800

    add CoordinatorLayout + ToolBar + TabLayout

--- app/src/main/java/com/imdevil/slackfish/MainActivity.java
+++ app/src/main/java/com/imdevil/slackfish/MainActivity.java
@@ -1,20 +1,15 @@
 package com.imdevil.slackfish;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.webkit.WebSettings;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-import org.jetbrains.annotations.NotNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * @author i-mde
@@ -22,46 +17,31 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private String pic = "https://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments&page=1";
-    private OkHttpClient mClient = new OkHttpClient();
+    private final String[] TABS = {"妹子图", "无聊图", "段子"};
+    private Toolbar mToolbar;
+    private ActionBar mActionBar;
+    private TabLayout mTabLayout;
+    private ViewPager2 mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mActionBar = getSupportActionBar();
+        mTabLayout = findViewById(R.id.tablayout);
+        mViewPager = findViewById(R.id.viewpager);
 
-        Request request = new Request.Builder()
-                .url(pic)
-                .build();
-        mClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                Log.d(TAG, "onResponse: " + response.body().string());
-            }
-        });
+        for (String title : TABS) {
+            mTabLayout.addTab(mTabLayout.newTab().setText(title));
+        }
     }
 
-    private String getUserAgent() {
-        String userAgent = "";
-        try {
-            userAgent = WebSettings.getDefaultUserAgent(getApplicationContext());
-        } catch (Exception e) {
-            userAgent = System.getProperty("http.agent");
-        }
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0, length = userAgent.length(); i < length; i++) {
-            char c = userAgent.charAt(i);
-            if (c <= '\u001f' || c >= '\u007f') {
-                sb.append(String.format("\\u%04x", (int) c));
-            } else {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }

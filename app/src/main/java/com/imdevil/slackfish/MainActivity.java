77d549308778c73a26ba74e91b995ac2a06bfc55
imdevil <516959708@qq.com>
2020-09-20 19:54:00 +0800

    first network

--- app/src/main/java/com/imdevil/slackfish/MainActivity.java
+++ app/src/main/java/com/imdevil/slackfish/MainActivity.java
@@ -3,15 +3,65 @@ package com.imdevil.slackfish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * @author i-mde
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private String pic = "https://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments&page=1";
+    private OkHttpClient mClient = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Request request = new Request.Builder()
+                .url(pic)
+                .build();
+        mClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
+    }
+
+    private String getUserAgent() {
+        String userAgent = "";
+        try {
+            userAgent = WebSettings.getDefaultUserAgent(getApplicationContext());
+        } catch (Exception e) {
+            userAgent = System.getProperty("http.agent");
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0, length = userAgent.length(); i < length; i++) {
+            char c = userAgent.charAt(i);
+            if (c <= '\u001f' || c >= '\u007f') {
+                sb.append(String.format("\\u%04x", (int) c));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }

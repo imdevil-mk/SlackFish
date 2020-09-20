dabab676242d1780372d142890f7cccceb954037
imdevil <516959708@qq.com>
2020-09-20 19:31:44 +0800

    init

---
+++ app/src/main/java/com/imdevil/slackfish/MainActivity.java
@@ -0,0 +1,17 @@
+package com.imdevil.slackfish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author i-mde
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
